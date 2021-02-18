0ee5263babc664ff48c4de0e9ed1fe988753e21b
Jen <jenhoang304@gmail.com>
2021-02-18 14:25:46 -0800

    update skills sections and change the dataset to make nest loop for skill categories and skill details list

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,27 +1,23 @@
 <template>
   <div class="bg-white customContainer q-pb-xl" id="skills">
     <h3 class="text-center q-ma-none">Skills</h3>
-    <div>
-      <h4 class="text-center skill-title">Front-end</h4>
-      <q-card class="my-card" v-for="(item, i) in frontEnd" :key="i">
+    <div v-for="(list, name) in skills" :key="name">
+      <h4 class="text-center skill-title">{{ name }}</h4>
+      <q-card class="my-card" v-for="(item, i) in list" :key="i">
         <q-card-section
           class="bg-primary row wrap justify-around items-center content-center"
         >
-          <h5 class="q-my-xs" v-for="(skill, j) in item" :key="j">
-            {{ skill.name }}
-          </h5>
-        </q-card-section>
-      </q-card>
-    </div>
-    <div>
-      <h4 class="text-center skill-title">Back-end</h4>
-      <q-card class="my-card" v-for="(item, i) in backEnd" :key="i">
-        <q-card-section
-          class="bg-primary row wrap justify-around items-center content-center"
-        >
-          <h5 class="q-my-xs" v-for="(skill, j) in item" :key="j">
-            {{ skill.name }}
-          </h5>
+          <div v-for="(skill, j) in item" :key="j">
+            <q-img
+              :src="skill.logo"
+              class="q-mt-auto skill-logo"
+              :id="skill.name"
+            />
+
+            <h5 class="q-my-xs text-center">
+              {{ skill.name }}
+            </h5>
+          </div>
         </q-card-section>
       </q-card>
     </div>
@@ -33,15 +29,31 @@ export default {
   name: "Skills",
   data() {
     return {
-      frontEnd: [
-        [{ name: "HTML" }, { name: "CSS" }, { name: "JavaScript" }],
-        [{ name: "React" }, { name: "Next" }],
-        [{ name: "Vue" }, { name: "Nuxt" }]
-      ],
-      backEnd: [
-        [{ name: "Django" }, { name: "Flask" }, { name: "Ruby on Rails" }],
-        [{ name: "SQL" }, { name: "MongoDB" }]
-      ]
+      skills: {
+        frontEnd: [
+          [
+            { name: "HTML", logo: "skills-icon/html.svg" },
+            { name: "CSS", logo: "skills-icon/css.svg" },
+            { name: "JavaScript", logo: "skills-icon/javascript.svg" }
+          ],
+          [
+            { name: "React", logo: "skills-icon/react.svg" },
+            { name: "Next", logo: "skills-icon/nextjs.svg" }
+          ],
+          [
+            { name: "Vue", logo: "skills-icon/vue.svg" },
+            { name: "Nuxt", logo: "skills-icon/nuxt.svg" }
+          ]
+        ],
+        backEnd: [
+          [
+            { name: "Django", logo: "skills-icon/django.svg" },
+            { name: "Flask", logo: "skills-icon/flask.svg" },
+            { name: "Ruby on Rails", logo: "skills-icon/ruby-on-rails.png" }
+          ],
+          [{ name: "SQL" }, { name: "MongoDB" }]
+        ]
+      }
     };
   }
 };
@@ -56,5 +68,15 @@ export default {
 }
 .skill-title {
   margin-bottom: 0.5em;
+}
+.skill-logo {
+  height: 60px;
+  width: 60px;
+}
+#Next {
+  width: 100px;
+}
+#Nuxt {
+  width: 80px;
 }
 </style>
